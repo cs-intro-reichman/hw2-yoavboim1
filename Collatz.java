e0580deb0de98b5b1304ddd1d9d5847c65b5447c
yoavboim <yoavboim1@gmail.com>
2025-11-07 19:06:23 +0200

    fix collatz

--- Collatz.java
+++ Collatz.java
@@ -1,41 +1,37 @@
 public class Collatz {
-    public static void main(String args[]) {
-        if (args.length != 2) {
-            System.out.println("Usage: java Collatz <N> <mode>");
-            return;
+
+    public static void main(String[] args) {
+        boolean verboseMode = args[1].equals("v");
+        int upperLimit = Integer.parseInt(args[0]);
+
+        for (int startNumber = 1; startNumber <= upperLimit; startNumber++) {
+            int currentNumber = startNumber;
+            int stepCount = 2;
+
+            if (verboseMode) System.out.print(String.format("%s ", currentNumber));
+            currentNumber = nextCollatz(currentNumber);
+
+            while (currentNumber != 1) {
+                if (verboseMode) System.out.print(String.format("%s ", currentNumber));
+                currentNumber = nextCollatz(currentNumber);
+                stepCount++;
+            }
+
+            if (verboseMode) System.out.print(String.format("%s ", currentNumber));
+            if (verboseMode) System.out.println(String.format("(%s)", stepCount));
         }
 
-        int N = Integer.parseInt(args[0]);
-        String mode = args[1];
+        System.out.println(String.format(
+            "Every one of the first %s hailstone sequences reached 1.", args[0]
+        ));
+    }
 
-        boolean verbose = mode.equals("v");
-
-        for (int seed = 1; seed <= N; seed++) {
-            int count = 1; // counts number of terms including the starting seed
-            int current = seed;
-
-            if (verbose) {
-                System.out.print(current + " ");
-            }
-
-            while (current != 1) {
-                if (current % 2 == 0) {
-                    current = current / 2;
-                } else {
-                    current = 3 * current + 1;
-                }
-
-                count++;
-                if (verbose) {
-                    System.out.print(current + " ");
-                }
-            }
-
-            if (verbose) {
-                System.out.println("(" + count + ")");
-            }
+    public static int nextCollatz(int number) {
+        if (number % 2 == 0) {
+            number = number / 2;
+        } else {
+            number = 3 * number + 1;
         }
-
-        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+        return number;
     }
 }
